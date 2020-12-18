2188a3b2c299bc0da7ae11f345c217cfa3ebf84a
doctarhyf <drrhyf@gmail.com>
2020-12-18 12:15:11 +0200

    done adding cropping features

--- app/src/main/java/com/example/croptest/MainActivity.java
+++ app/src/main/java/com/example/croptest/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.croptest;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.tbruyelle.rxpermissions3.RxPermissions;
 import com.yalantis.ucrop.UCrop;
@@ -17,6 +21,9 @@ public class MainActivity extends AppCompatActivity {
 
     private String TAG = "TAG";
     private String perms[] = new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.CAMERA};
+    private String sourcePath;
+    private String destPath;
+    private ImageView iv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,24 +32,22 @@ public class MainActivity extends AppCompatActivity {
 
         askPermissions();
 
+        iv = findViewById(R.id.iv);
+        sourcePath = getExternalFilesDir(Environment.DIRECTORY_PICTURES).getPath() + "/crop.jpg";
+        destPath = getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS).getPath() + "/crop.jpg";
 
-        String sourcePath = getExternalFilesDir(Environment.DIRECTORY_PICTURES).getPath() + "/crop.jpg";
-        String destPath = getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS).getPath() + "/crop.jpg";
+
+        iv.setImageURI(Uri.fromFile(new File(sourcePath)));
 
         Log.e(TAG, "dbg -> sourcePath : " + sourcePath );
         Log.e(TAG, "dbg -> destPath : " + destPath );
 
 
-        Uri sourceUri = Uri.fromFile(new File(sourcePath));
-        Uri destinationUri = Uri.fromFile(new File(destPath));
-        int maxWidth = 420;
-        int maxHeight = 420;
-        UCrop.of(sourceUri, destinationUri)
-                .withAspectRatio(16, 9)
-                .withMaxResultSize(maxWidth, maxHeight)
-                .start(this);
+
 
     }
+
+
 
     private void askPermissions() {
         new RxPermissions(this)
@@ -59,6 +64,32 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
 
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == UCrop.REQUEST_CROP) {
+            final Uri resultUri = UCrop.getOutput(data);
+
+            iv.setImageURI(resultUri);
+
+        } else if (resultCode == UCrop.RESULT_ERROR) {
+            final Throwable cropError = UCrop.getError(data);
+        }
+    }
+
+    public void onCrop(View view) {
+
+        Uri sourceUri = Uri.fromFile(new File(sourcePath));
+        Uri destinationUri = Uri.fromFile(new File(destPath));
+        int maxWidth = 420;
+        int maxHeight = 420;
+        UCrop.of(sourceUri, destinationUri)
+                .withAspectRatio(16, 9)
+                .withMaxResultSize(maxWidth, maxHeight)
+                .start(this);
 
     }
 }
